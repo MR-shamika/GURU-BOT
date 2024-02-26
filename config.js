5e023ad6d342784628c093a6d455d937182691f0
Shamika Harshamal <158842737+MR-shamika@users.noreply.github.com>
2024-02-26 08:40:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['94777456024', 'Mr.Shamika', true],
+  ['94757807978', 'MY WHATSAPP BOT', true], 
   [''] 
 ] //Number of owners
 
@@ -50,10 +50,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = 'ꜱʜᴀᴍɪᴋᴀ-ᴍᴅ ☫'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
+global.packname = '𝚂𝙷𝙰𝙼𝙸𝙺𝙰-𝙼𝙳┃ᴮᴼᵀ' 
+global.author = '𝐌𝐑.𝐒𝐇𝐀𝐌𝐈𝐊𝐀' 
 global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
 global.dygp = 'https://chat.whatsapp.com/BFfD1C0mTDDDfVdKPkxRAA'
